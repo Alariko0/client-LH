d8d11ec839916173169065c71f3b078ff806b5d2
Alariko0 <hector.mario86@gmail.com>
2022-12-11 23:29:25 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import BasicExample from './components/navBar/NavBar';
 import Home from './pages/homePage'
+import Login from './pages/logInPage';
+import Signup from './pages/signUpPage';
 import TodoList from './pages/todoListPage';
 
 
@@ -9,9 +12,12 @@ import TodoList from './pages/todoListPage';
 function App() {
   return (
     <div>
+      <BasicExample />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/gallery' element={<TodoList />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
