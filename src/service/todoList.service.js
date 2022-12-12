3cfe4da40d749d4e7f0a82e0680988cdde57819a
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-12-12 11:45:19 +0100

    .

--- src/service/todoList.service.js
+++ src/service/todoList.service.js
@@ -6,10 +6,30 @@ class TodoListAPI {
             baseURL: 'http://localhost:5005/todolist'
         })
     }
-    // Cada una de las rutas que vmos a utilizar
+    // Cada una de las rutas que vamos a utilizar
 
     getAllTodo() {
         return this.axios('/todolist').then((response) => response.data)
+    }
+
+    getOneTodoListById(id) {
+        return this.axios.get(`/${id}`).then((response) => response.data);
+    }
+
+    createTodoList(body) {
+        return this.axios.post(`/`, body).then((response) => response.data);
+    }
+
+    updateOneTodoListById(id, body) {
+        return this.axios
+            .put(`/${id}`, body)
+            .then((response) => response.data);
+    }
+
+    deleteOneTodoListById(id) {
+        return this.axios
+            .delete(`/${id}`)
+            .then((response) => response.data);
     }
 
 }
