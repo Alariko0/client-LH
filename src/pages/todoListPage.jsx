85f3ea44ae494924e46e231ed9a340c2f1677025
Alariko0 <hector.mario86@gmail.com>
2022-12-14 22:41:37 +0100

    .delete

--- src/pages/todoListPage.jsx
+++ src/pages/todoListPage.jsx
@@ -1,17 +1,36 @@
 import { useEffect, useState } from "react"
-import { Container, Row } from "react-bootstrap"
+import { Button, Container, Row } from "react-bootstrap"
 import TodoListAPI from "../service/todoList.service"
+
 
 
 const TodoList = () => {
     const [todoLists, setTodoLists] = useState([])
 
     useEffect(() => {
+
         TodoListAPI.getAllTodoList().then((todoList) => {
 
             setTodoLists(todoList)
         })
     }, [])
+
+    const deletList = (id) => {
+        TodoListAPI.deleteOneTodoListById(id).then(() => updatePage())
+
+    }
+
+    const updatePage = () => {
+        TodoListAPI.getAllTodoList().then((todoList) => {
+
+            setTodoLists(todoList)
+        })
+    }
+
+    const updateList = () => {
+        TodoListAPI.updateOneTodoListById()
+    }
+
 
 
     return (
@@ -19,17 +38,19 @@ const TodoList = () => {
             <Container>
                 <Row>
 
-                    {todoLists.map((todoLists) => {
-                        console.log(todoLists.tasks.room);
+                    {todoLists.map((List) => {
                         return (
 
-                            <div key={todoLists._id}>
-                                <p><strong>{todoLists.name}</strong></p><br></br>
-                                <p>{todoLists.tasks.room.electrician?.job}</p><br></br>
-                                <p>{todoLists.tasks.room.plumber?.job}</p><br></br>
-                                <p>{todoLists.tasks.room.constructionWorker.job?.walls.task}</p><br></br>
-                                <p>{todoLists.tasks.room.constructionWorker.job?.floor.task}</p><br></br>
-                                <p>{todoLists.tasks.room.constructionWorker.job?.ceiling?.task}</p><br></br>
+                            <div key={List._id}>
+                                <p><strong>{List.name}</strong></p><br></br><br></br>
+                                <p><strong>Electricista :<br></br><br></br></strong><input type="checkbox" /> {List.tasks.room.electrician?.job}</p><br></br>
+                                <p><strong>Fontanero :<br></br><br></br></strong><input type="checkbox" /> {List.tasks.room.plumber?.job}</p><br></br>
+                                <p><strong>Albañil :<br></br><br></br></strong><input type="checkbox" /> {List.tasks.room.constructionWorker.job?.walls.task}</p><br></br>
+                                <p><input type="checkbox" /> {List.tasks.room.constructionWorker.job?.floor.task}</p><br></br>
+                                <p><input type="checkbox" /> {List.tasks.room.constructionWorker.job?.ceiling?.task}</p><br></br>
+
+                                <Button onClick={() => deletList(List._id)} >Eliminar</Button>
+                                <Button onClick={updateList} >Modificar</Button>
                             </div>
 
                         )
