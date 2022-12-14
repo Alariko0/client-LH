9094f03162396f8489e5b73f21c30064af78153e
Alariko0 <hector.mario86@gmail.com>
2022-12-14 16:39:42 +0100

    debug form create

--- src/pages/todoListCreatePage.jsx
+++ src/pages/todoListCreatePage.jsx
@@ -4,7 +4,7 @@ import todoListApi from '../service/todoList.service'
 import { useNavigate } from 'react-router-dom'
 const TodoListCreate = () => {
     const navigate = useNavigate()
-    const [todoList, setTodoList] = useState({})
+    const [todoList, setTodoList] = useState({ tasks: { room: { constructionWorker: { job: {} } } } })
     const createNewTodoList = (event) => {
         event.preventDefault();
         todoListApi
@@ -17,6 +17,26 @@ const TodoListCreate = () => {
         const { name, value } = event.target
         setTodoList({ ...todoList, [name]: value })
     }
+
+    const updateNewTodoListTask = (event, target) => {
+        const { name, value } = event.target
+        const _todoList = JSON.parse(JSON.stringify(todoList));
+        _todoList.tasks.room[target] = { ..._todoList.tasks.room[target], [name]: value }
+        console.log(_todoList);
+        setTodoList(_todoList);
+    }
+
+    const updateNewTodoListConstructionWorker = (event, target) => {
+        const { name, value } = event.target
+        const _todoList = JSON.parse(JSON.stringify(todoList));
+        _todoList.tasks.room.constructionWorker.job[target] = { ..._todoList.tasks.room.constructionWorker.job[target], [name]: value }
+        console.log(_todoList);
+        setTodoList(_todoList);
+    }
+
+
+
+
     return (
         <>
             <div style={{ marginTop: '60px' }}></div>
@@ -26,7 +46,7 @@ const TodoListCreate = () => {
                     <Form.Control
                         onChange={updateNewTodoList}
                         type='text'
-                        name='-----'
+                        name='name'
                         placeholder='----'
                     />
                 </Form.Group>
@@ -47,15 +67,51 @@ const TodoListCreate = () => {
                     />
                 </Form.Group>} */}
                 <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
-                    <Form.Label>TASKS</Form.Label>
+                    <Form.Label>PLUMBER</Form.Label>
                     <Form.Control
                         type='text'
                         placeholder='-------'
-                        name='---------'
-                        onChange={updateNewTodoList}
+                        name='job'
+                        onChange={(e) => updateNewTodoListTask(e, 'plumber')}
                     />
                 </Form.Group>
-                <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+                    <Form.Label>ELECTRICIAN</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='-------'
+                        name='job'
+                        onChange={(e) => updateNewTodoListTask(e, 'electrician')}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+                    <Form.Label>WALLS</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='-------'
+                        name='task'
+                        onChange={(e) => updateNewTodoListConstructionWorker(e, 'walls')}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+                    <Form.Label>FLOOR</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='-------'
+                        name='task'
+                        onChange={(e) => updateNewTodoListConstructionWorker(e, 'floor')}
+                    />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+                    <Form.Label>CEILING</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='-------'
+                        name='task'
+                        onChange={(e) => updateNewTodoListConstructionWorker(e, 'ceiling')}
+                    />
+                </Form.Group>
+                {/* <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
                     <Form.Label>WORKERS</Form.Label>
                     <Form.Control
                         as='textarea'
@@ -63,7 +119,7 @@ const TodoListCreate = () => {
                         name='--------'
                         onChange={updateNewTodoList}
                     />
-                </Form.Group>
+                </Form.Group> */}
                 <Button variant='primary' type='submit'>
                     Create
                 </Button>
