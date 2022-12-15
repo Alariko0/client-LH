f5c6da3d61e221c8fac20bf0a9f89876f8273d7d
Alariko0 <hector.mario86@gmail.com>
2022-12-15 20:25:14 +0100

    checkbox

--- src/components/checkbox/checkbox.jsx
+++ src/components/checkbox/checkbox.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
-
-const Checkbox = ({ initialState }) => {
+const Checkbox = ({ initialState, setTodoList, worker, subwork }) => {
     const [checked, setChecked] = useState(initialState);
-
-    const onClick = (checked) => {
-        setChecked(checked);
-
+    const onClick = (checkboxChecked) => {
+        setChecked(checkboxChecked);
+        if (worker === 'constructionWorker') {
+            setTodoList(todoList => {
+                const _todoList = { ...todoList }
+                _todoList.tasks.room[worker].job[subwork].completed = checkboxChecked
+                return _todoList
+            })
+        } else {
+            setTodoList(todoList => {
+                const _todoList = { ...todoList }
+                _todoList.tasks.room[worker].completed = checkboxChecked
+                return _todoList
+            })
+        }
         // onChange(id, checked);
     }
     return (
@@ -16,5 +25,4 @@ const Checkbox = ({ initialState }) => {
         />
     )
 }
-
 export default Checkbox
--- src/pages/todoListUpdatePage.jsx
+++ src/pages/todoListUpdatePage.jsx
@@ -1,79 +1,79 @@
-import React from 'react';
-import { Button, Form } from 'react-bootstrap';
-import todoListApi from '../service/todoList.service';
-
-return (
-    <>
-        <div style={{ marginTop: '60px' }}></div>
-        <Form onSubmit={this.handleSubmit}>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-                <Form.Label>DIRECCION</Form.Label>
-                <Form.Control
-                    onChange={this.handleNameChange}
-                    type='text'
-                    name='name'
-                    placeholder='----'
-                />
-            </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
-                <Form.Label>HABITACION</Form.Label>
-                <Form.Control
-                    onChange={this.handleSubmit}
-                    type='text'
-                    name='room'
-                    placeholder='----'
-                />
-            </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
-                <Form.Label>FONTANERO</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='-------'
-
-                />
-            </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
-                <Form.Label>ELECTRICISTA</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='-------'
-                    name='job'
-                //onChange={(e) => updateNewTodoListTask(e, 'electrician')}
-                />
-            </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
-                <Form.Label>PAREDES</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='-------'
-                    name='task'
-                //onChange={(e) => updateNewTodoListConstructionWorker(e, 'walls')}
-                />
-            </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
-                <Form.Label>SUELO</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='-------'
-                    name='task'
-                //onChange={(e) => updateNewTodoListConstructionWorker(e, 'floor')}
-                />
-            </Form.Group>
-            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
-                <Form.Label>TECHO</Form.Label>
-                <Form.Control
-                    type='text'
-                    placeholder='-------'
-                    name='task'
-                //onChange={(e) => updateNewTodoListConstructionWorker(e, 'ceiling')}
-                />
-            </Form.Group>
-            <Button variant='primary' type='submit'>
-                Modificar
-            </Button>
-        </Form>
-    </>
-);
-
-export default EditForm
+//import React from 'react';
+//import { Button, Form } from 'react-bootstrap';
+//import todoListApi from '../service/todoList.service';
+//
+//return (
+//    <>
+//        <div style={{ marginTop: '60px' }}></div>
+//        <Form onSubmit={this.handleSubmit}>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+//                <Form.Label>DIRECCION</Form.Label>
+//                <Form.Control
+//                    onChange={this.handleNameChange}
+//                    type='text'
+//                    name='name'
+//                    placeholder='----'
+//                />
+//            </Form.Group>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+//                <Form.Label>HABITACION</Form.Label>
+//                <Form.Control
+//                    onChange={this.handleSubmit}
+//                    type='text'
+//                    name='room'
+//                    placeholder='----'
+//                />
+//            </Form.Group>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+//                <Form.Label>FONTANERO</Form.Label>
+//                <Form.Control
+//                    type='text'
+//                    placeholder='-------'
+//
+//                />
+//            </Form.Group>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+//                <Form.Label>ELECTRICISTA</Form.Label>
+//                <Form.Control
+//                    type='text'
+//                    placeholder='-------'
+//                    name='job'
+//                onChange={(e) => updateNewTodoListTask(e, 'electrician')}
+//                />
+//            </Form.Group>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+//                <Form.Label>PAREDES</Form.Label>
+//                <Form.Control
+//                    type='text'
+//                    placeholder='-------'
+//                    name='task'
+//                onChange={(e) => updateNewTodoListConstructionWorker(e, 'walls')}
+//                />
+//            </Form.Group>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+//                <Form.Label>SUELO</Form.Label>
+//                <Form.Control
+//                    type='text'
+//                    placeholder='-------'
+//                    name='task'
+//                onChange={(e) => updateNewTodoListConstructionWorker(e, 'floor')}
+//                />
+//            </Form.Group>
+//            <Form.Group className='mb-3' controlId='exampleForm.ControlInput3'>
+//                <Form.Label>TECHO</Form.Label>
+//                <Form.Control
+//                    type='text'
+//                    placeholder='-------'
+//                    name='task'
+//                onChange={(e) => updateNewTodoListConstructionWorker(e, 'ceiling')}
+//                />
+//            </Form.Group>
+//            <Button variant='primary' type='submit'>
+//                Modificar
+//            </Button>
+//        </Form>
+//    </>
+//);
+//
+//export default EditForm
 
