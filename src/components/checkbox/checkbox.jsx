1c89b50b59e9dcd456649c1eb2bbb27c6c8abc05
Alariko0 <hector.mario86@gmail.com>
2022-12-15 21:12:57 +0100

    prueba

--- src/components/checkbox/checkbox.jsx
+++ src/components/checkbox/checkbox.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
 const Checkbox = ({ initialState, setTodoList, worker, subwork }) => {
     const [checked, setChecked] = useState(initialState);
+
     const onClick = (checkboxChecked) => {
         setChecked(checkboxChecked);
         if (worker === 'constructionWorker') {
@@ -8,6 +11,7 @@ const Checkbox = ({ initialState, setTodoList, worker, subwork }) => {
                 _todoList.tasks.room[worker].job[subwork].completed = checkboxChecked
                 return _todoList
             })
+
         } else {
             setTodoList(todoList => {
                 const _todoList = { ...todoList }
@@ -15,14 +19,14 @@ const Checkbox = ({ initialState, setTodoList, worker, subwork }) => {
                 return _todoList
             })
         }
+
         // onChange(id, checked);
     }
-    return (
-        <input
-            type="checkbox"
-            onChange={e => onClick(e.target.checked)}
-            checked={checked}
-        />
+    return (<input
+        type="checkbox"
+        onChange={e => onClick(e.target.checked)}
+        checked={checked}
+    />
     )
 }
 export default Checkbox
